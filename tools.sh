477c3b012876086d2f5a37a36ce1b08d3a0d4868
Yanis <yanis.deluca@distrilog.net>
2022-11-25 14:00:01 +0100

    master : test simple qsd qsd

--- tools.sh
+++ tools.sh
@@ -223,7 +223,7 @@ commit () {
 		# printf $BIPurple"Message : $On_Green $argList $Color_Off\n"
 		printf $BIPurple"Message : $On_Green $argList $Color_Off\n\n"
 		message=$cmd "$branch : $argList"
-		echo $message
+		$cmd "$message"
 	else
 		printf $BIRed"Le message de commit est vide !"$Color_Off
 	fi
