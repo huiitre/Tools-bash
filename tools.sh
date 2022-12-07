d0ce365aad24b44f9f5136789740f465a8f13cce
Yanis <yanis.deluca@distrilog.net>
2022-12-07 13:58:24 +0100

    : ajout d une regex dans la fonction commit afin de ne commiter que le numéro de ticket et non le nom de la branche au complet

--- tools.sh
+++ tools.sh
@@ -212,7 +212,6 @@ commit () {
 	local branch=$(git symbolic-ref HEAD --short 2> /dev/null)
 	#* On récupère le numéro du ticket avec le # en plus
 	local ticket=$(echo $branch | grep '#.*' -o)
-	echo $ticket
 	#* On récupère le nombre d'arguments de la fonction
 	local argTotal=$#
 	#* La liste des arguments
